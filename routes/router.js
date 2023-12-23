514d668425209bc631b9d8f72fae5c6c35c13116
40d10n <153600546+40d10n@users.noreply.github.com>
2023-12-23 15:52:49 +0200

    Update router.js
    Зміни мову сайту

--- routes/router.js
+++ routes/router.js
@@ -7,64 +7,64 @@ const todosService = new TodosService();
 
 router.get('/', async (req, res, next) => {
 
-    const listSrc = await todosService.getList();
+  const listSrc = await todosService.getList();
 
-    const list = listSrc.map((el) => {
-        return {
-            name: el.name,
-            id: el.id.toString(),
-            completed: el.completed,
-            createdAt: new Date(el.createdAt).toLocaleString('ru-RU'),
-            completedClass: el.completed ? 'todos__item_complited' : '',
-        };
-    });
+  const list = listSrc.map((el) => {
+    return {
+      name: el.name,
+      id: el.id.toString(),
+      completed: el.completed,
+      createdAt: new Date(el.createdAt).toLocaleString('ru-RU'),
+      completedClass: el.completed ? 'todos__item_complited' : '',
+    };
+  });
 
-    res.render('index', {
-        title: 'Список дел',
-        description: 'Приложение - список дел',
-        h1: 'Список дел',
-        text: 'Привет! Давай составим список дел, <br>чтобы ничего не забыть!',
-        list,
-    });
+  res.render('index', {
+    title: 'Список справ',
+    description: 'Приложение - список справ',
+    h1: 'Список справ',
+    text: 'Привіт! Запиши справу <br>щоб нічого не забути!',
+    list,
+  });
 });
 
 
 router.get('/new', async (req, res, next) => {
-    res.render('new', {
-        title: 'Создать новое дело',
-        description: 'Создать новое дело в приложении Список дел',
-        h1: 'Создать новое дело',
-        text: 'Привет! Давай создадим новое дело, <br>чтобы не забыть его!',
-    });
+  res.render('new', {
+    title: 'Створи нову справу',
+    description: 'Створи нову справу в приложении Список дел',
+    h1: 'Створи нову справу',
+    text: 'Привіт! Запиши справу <br>щоб нічого не забути!',
+  });
 
 });
 
 
 router.post('/add', async (req, res, next) => {
-    if(!req.body)   res.redirect('/');
+  if(!req.body)   res.redirect('/');
 
 
-    const { text } = req.body;
-    const newTask = await todosService.addTask(text);
-    res.redirect('/');
+  const { text } = req.body;
+  const newTask = await todosService.addTask(text);  
+  res.redirect('/');
 
 });
 
 router.post('/complete', async (req, res, next) => {
 
-    const { id } = req.body;
+  const { id } = req.body;
 
-    await todosService.toggleComplete(id);
+  await todosService.toggleComplete(id);
 
-    res.redirect('/');
+  res.redirect('/');
 
 });
 
 router.post('/delete', async (req, res, next) => {
-    const { id } = req.body;
+  const { id } = req.body;
 
-    const newTask = await todosService.delete(id);
-    res.redirect('/');
+  const newTask = await todosService.delete(id);
+  res.redirect('/');
 
 });
 
